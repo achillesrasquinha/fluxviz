f6ef2678c0e7b61f2ae6bc410f9ba00eb82010e8
Achilles Rasquinha <achillesrasquinha@gmail.com>
2021-05-10 11:27:26 -0500

    fix: Updated setup.py, support python versions

--- setup.py
+++ setup.py
@@ -143,7 +143,6 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
-        
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: PyPy"
     ],
